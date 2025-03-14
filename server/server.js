733268ff48abe603018232a54b3415b533e02e92
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-03-14 12:13:21 +0100

    update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 const morgan = require("morgan");
 const connectDB = require("./config/database");
 const app = express();
@@ -12,14 +13,31 @@ const userRoutes = require("./routes/userRoutes");
 
 // Frontend url here
 const corsOptions = {
-  //   origin: "http://localhost:3000",
+  // origin: "PROCESS.env.CLIENT_URL",
   credentials: true,
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
 };
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'https://apis.google.com'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "https://yourcdn.com"],
+        contentSrc: ["'self'"],
+        fontSrc: ["'self'"],
+      },
+    },
+    XssFilter: true, //Enable X-XSS Filter Protection Header
+  })
+);
 
 // Connect to Database
 connectDB();
