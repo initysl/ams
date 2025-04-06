96c2377d178f48233c656e9d615a99fa89dc593b
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-04-06 03:59:43 +0100

    updates

---
+++ server/utils/multerConfig.js
@@ -0,0 +1,36 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueName);
+  },
+});
+
+// File filter to allow only specific file types
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|gif/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  }
+  cb(
+    "Error: File upload only supports the following filetypes - " + allowedTypes
+  );
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 10 }, // Limit file size to 10MB
+  fileFilter: fileFilter,
+});
+
+module.exports = upload;
