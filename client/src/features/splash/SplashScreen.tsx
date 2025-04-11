8e010411f7d4f65da82d6b15bc82dce19f1d9145
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-04-11 16:54:27 +0100

    update-splashscreen

--- client/src/features/splash/SplashScreen.tsx
+++ client/src/features/splash/SplashScreen.tsx
@@ -1,31 +1,67 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import logo from "../../assets/images/at.png";
-import React from "react";
+import { useEffect, useState } from "react";
+
+const messages: string[] = [
+  "Dive into the world of automated attendance management",
+  "Say goodbye to manual records and hello to efficiency!",
+  "Seamlessly track attendance in real-time with just a click.",
+];
 
 const SplashScreen: React.FC = () => {
+  const [index, setIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % messages.length);
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="splash-screen space-y-4 flex flex-col items-center justify-center min-h-svh">
       {/* Logo and Title */}
-      <div className="fixed top-5">
-        <h1 className="text-3xl font-bold text-green-600 mb-4">AttendEase</h1>
+      <div className="relative -top-7 ">
+        <h1 className="text-3xl font-bold text-green-600 ">AttendEase</h1>
       </div>
-      <div className="mt-5 lg:mt-10">
+      <motion.div
+        className="rounded-2xl"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.4,
+          scale: {
+            type: "spring",
+            stiffness: 100,
+            visualDuration: 0.4,
+            bounce: 0.5,
+          },
+        }}
+      >
         <img src={logo} alt="App logo" className="max-w-sm" />
-      </div>
-      <div className="text-center mt-4 lg:mt-0">
-        <p className="text-lg font-bold mb-4">
-          Your attendance management solution{" "}
-        </p>
-        <p className="font-light">
-          Get ready to dive in the life of automated attendance management
-        </p>
+      </motion.div>
+      <div className="text-center text-pretty space-y-4">
+        <p className="text-lg font-bold">Your Attendance Management Solution</p>
 
-        {/* Buttons */}
-        <div>
-          <Button className="bg-green-600 text-white mt-10 cursor-pointer">
-            Get Started
-          </Button>
-        </div>
+        {/* Animated paragraph */}
+        <AnimatePresence mode="popLayout">
+          <motion.p
+            key={index}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.6 }}
+            className="font-light"
+          >
+            {messages[index]}
+          </motion.p>
+        </AnimatePresence>
+
+        <Button className="bg-green-600 hover:bg-green-800 hover:ease-in-out text-white cursor-pointer">
+          Get Started
+        </Button>
       </div>
     </div>
   );
