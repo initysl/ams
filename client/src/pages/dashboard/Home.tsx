9c5269dcd18fcd2d0b4637d6145d069cdc488cd9
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-04-20 09:54:47 +0100

    update

--- client/src/pages/dashboard/Home.tsx
+++ client/src/pages/dashboard/Home.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Card,
   CardHeader,
@@ -6,22 +7,55 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
-import React from "react";
+
+import * as Icons from "lucide-react";
+import rawCardData from "@/components/json/card.json";
+
+// Define TypeScript type for each card item
+type CardItem = {
+  id: number;
+  title: string;
+  value: string;
+  description: string;
+  icon: keyof typeof Icons;
+};
+
+// Cast the JSON import with proper typing
+const cardData: { cards: CardItem[] } = rawCardData as { cards: CardItem[] };
+
+const Icon = ({ name }: { name: keyof typeof Icons }) => {
+  const LucideIcon = Icons[name];
+  return LucideIcon ? (
+    <LucideIcon className="h-10 w-10 text-primary bg-white shadow p-2 rounded-2xl" />
+  ) : null;
+};
 
 const Home: React.FC = () => {
   return (
-    <Card className="w-96 bg-white shadow-lg">
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
+    <div>
+      <Card className=" bg-green-700 text-white p-10 mb-5">
+        <h2 className="font-bold text-2xl">Welcome Back @name</h2>
+        <div>
+          <p>Your ultimate attendance management solution</p>
+        </div>
+      </Card>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {cardData.cards.map((text) => (
+          <Card key={text.id} className="bg-white shadow-lg">
+            <CardHeader>
+              <div className="flex justify-between">
+                <CardTitle>{text.title}</CardTitle>
+                <Icon name={text.icon} />
+              </div>
+              <CardDescription>{text.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="text-gray-700 font-semibold">{text.value}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
   );
 };
 
