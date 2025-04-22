6b3664b18b8a276fb1f2f876be6c2cdbcf0858f3
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-04-22 19:28:46 +0100

    update

--- client/src/pages/auth/ResetPass.tsx
+++ client/src/pages/auth/ResetPass.tsx
@@ -19,12 +19,11 @@ import { EyeIcon, EyeOff } from "lucide-react";
 const resetPassSchema = z
   .object({
     password: z.string().min(6, "Password must be at least 6 characters"),
-    confirmPassword: z
-      .string()
-      .min(6, "Confirm Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm your password"),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
+    path: ["confirmPassword"],
   });
 
 type ResetPassForm = z.infer<typeof resetPassSchema>;
