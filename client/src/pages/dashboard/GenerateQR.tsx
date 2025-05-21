cad408aaed8e651682da4e123f42ff0470b32df4
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-05-21 14:54:13 +0100

    update

--- client/src/pages/dashboard/GenerateQR.tsx
+++ client/src/pages/dashboard/GenerateQR.tsx
@@ -14,7 +14,7 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-const qrSchema = {
+const qrSchema = z.object({
   courseTitle: z.string().min(5, "Course title is required"),
   courseCode: z.string().min(5, "Course code is required"),
   courseLevel: z.enum(["100", "200", "300", "400", "500"], {
@@ -24,7 +24,7 @@ const qrSchema = {
     .number()
     .min(1, "Duration must be at least 1 minute")
     .max(60, "Duration must be at most 60 minutes"),
-};
+});
 
 type GenerateQR = {
   courseTitle: string;
@@ -33,67 +33,72 @@ type GenerateQR = {
   duration: number;
 };
 
+type GenerateFields = z.infer<typeof qrSchema>;
+
 const GenerateQR = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<GenerateFields>({
+    resolver: zodResolver(qrSchema),
+  });
+
   return (
     <div>
       <Card className="max-w-4xl mx-auto bg-white p-5">
         <CardDescription>
-          <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
-            <div className="space-y-2 mb-2">
+          <div className="space-y-6">
+            <div className="space-y-2">
               <Label htmlFor="courseTitle">Course Title</Label>
               <Input
                 id="courseTitle"
                 type="text"
                 placeholder="Enter course title"
+                {...register("courseTitle")}
               />
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
-              <div className="space-y-2 mb-2">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <div className="space-y-2">
                 <Label htmlFor="courseCode">Course Code</Label>
                 <Input
                   id="courseCode"
                   type="text"
                   placeholder="Enter course code"
+                  {...register("courseCode")}
                 />
               </div>
-              <div className="grid grid-cols-2 md:flex gap-4 place-items-center">
-                <div className="space-y-2 mb-2">
-                  <Label htmlFor="courseLevel">Course Level</Label>
-                  <Select defaultValue="400">
-                    <SelectTrigger className="w-[150px] md:w-[180px] bg-gray-100 border border-gray-300 rounded-md">
-                      <SelectValue placeholder="Choose level" />
-                    </SelectTrigger>
-                    <SelectContent className="bg-white border border-gray-300 rounded-md">
-                      <SelectGroup>
-                        <SelectItem value="100" className="hover:bg-gray-200">
-                          100
+              <div className="space-y-2">
+                <Label htmlFor="courseLevel">Course Level</Label>
+                <Select defaultValue="400">
+                  <SelectTrigger className="w-full bg-gray-100 border border-gray-300 rounded-md">
+                    <SelectValue placeholder="Choose level" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-white border border-gray-300 rounded-md">
+                    <SelectGroup>
+                      {["100", "200", "300", "400", "500"].map((level) => (
+                        <SelectItem
+                          key={level}
+                          value={level}
+                          className="hover:bg-gray-200"
+                        >
+                          {level}
                         </SelectItem>
-                        <SelectItem value="200" className="hover:bg-gray-200">
-                          200
-                        </SelectItem>
-                        <SelectItem value="300" className="hover:bg-gray-200">
-                          300
-                        </SelectItem>
-                        <SelectItem value="400" className="hover:bg-gray-200">
-                          400
-                        </SelectItem>
-                        <SelectItem value="500" className="hover:bg-gray-200">
-                          500
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="space-y-2 mb-2">
-                  <Label htmlFor="duration">Duration(minutes)</Label>
-                  <Input
-                    id="duration"
-                    type="number"
-                    min={1}
-                    max={60}
-                    placeholder="20"
-                  />
-                </div>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="duration">Duration (minutes)</Label>
+                <Input
+                  id="duration"
+                  type="number"
+                  min={1}
+                  max={60}
+                  placeholder="20"
+                  {...register("duration", { valueAsNumber: true })}
+                />
               </div>
             </div>
           </div>
