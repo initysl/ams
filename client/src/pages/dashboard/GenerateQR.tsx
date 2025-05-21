e0282e300d49440db4d548ca131d7a447de8017a
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-05-21 15:59:35 +0100

    update

--- client/src/pages/dashboard/GenerateQR.tsx
+++ client/src/pages/dashboard/GenerateQR.tsx
@@ -158,7 +158,7 @@ const GenerateQR = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-4 space-y-6">
+    <div className="max-w-4xl mx-auto space-y-6">
       <form onSubmit={handleSubmit(handleGenerateQR)} className="space-y-6">
         <Card className="bg-white p-5">
           <CardDescription>
