7df94f692dd9188c38cae21cd859db251f62455b
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-05-24 10:44:46 +0100

    update

---
+++ client/src/pages/dashboard/Student/MarkPopover.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  PopoverTrigger,
+  Popover,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Label } from "@/components/ui/label";
+import { QrCode } from "lucide-react";
+
+interface MarkPopoverProps {
+  triggerText?: string;
+}
+
+const MarkPopover: React.FC<MarkPopoverProps> = ({
+  triggerText = "Open Popover",
+}) => {
+  return (
+    <div>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button className="bg-teal-600 hover:bg-teal-700 text-white font-semibold p-5 w-full max-w-sm shadow-lg cursor-pointer">
+            {triggerText}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-80 bg-white border-amber-50">
+          <div className="space-y-6 p-6">
+            <div className="flex justify-center">
+              <QrCode size={72} />
+            </div>
+
+            <div className="space-y-4">
+              <div className="flex justify-between items-start">
+                <div>
+                  <p className="text-sm text-gray-500">Course Code</p>
+                  <span className="font-semibold ">CSC 309</span>
+                </div>
+                <div>
+                  <p className="text-sm text-gray-500">Time</p>
+                  <span className="font-semibold ">11:30AM</span>
+                </div>
+              </div>
+
+              <div>
+                <p className="text-sm text-gray-500">Course Title</p>
+                <span className="font-semibold  uppercase block">
+                  Introduction to Python
+                </span>
+              </div>
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+export default MarkPopover;
--- client/src/pages/dashboard/Student/ScanQR.tsx
+++ client/src/pages/dashboard/Student/ScanQR.tsx
@@ -10,6 +10,8 @@ import {
 } from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import MarkPopover from "./MarkPopover";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 
 const ScanQR = () => {
   const [isScanning, setIsScanning] = useState(false);
@@ -21,36 +23,22 @@ const ScanQR = () => {
       <p className="text-center text-pretty">
         Scan QR Code by using your camera or uploading to mark attendance
       </p>
-      <div className="mt-20">
-        <Card className="flex flex-col items-center justify-center p-5 mx-w-4xl">
+      <div className="mt-20 ">
+        <Card className="flex flex-col items-center justify-center p-5 ">
           <div className="flex items-center justify-center w-64 h-64 border-2 border-dashed rounded-lg">
             {isScanning ? <Camera size={80} /> : <ScanQrCode size={80} />}
           </div>
         </Card>
 
-        <div className="flex flex-col items-center justify-center mt-10 space-y-8">
-          <div className="flex items-center justify-center mt-4 gap-5">
-            <Button
-              variant="outline"
-              className="border-2 border-teal-600 hover:bg-teal-700 hover:text-white"
-            >
-              Upload <Upload />{" "}
-            </Button>
-            <Button
-              variant="outline"
-              className="border-2 border-teal-600 hover:bg-teal-700 hover:text-white"
-            >
-              Camera <Camera />{" "}
-            </Button>
-          </div>
-
-          <Button className="bg-teal-600 hover:bg-teal-700 text-white font-semibold p-5 w-full max-w-sm shadow-lg cursor-pointer">
-            SCAN
-          </Button>
+        <div className="flex flex-col items-center justify-center mt-16 ">
+          <Popover>
+            <PopoverTrigger asChild>
+              <MarkPopover triggerText="SCAN QR CODE" />
+            </PopoverTrigger>
+          </Popover>
         </div>
       </div>
     </div>
   );
 };
-
 export default ScanQR;
