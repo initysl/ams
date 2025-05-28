520b92d5321c52d2412a730e8bb3d1446cc03c5a
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-05-28 20:21:47 +0100

    update

--- client/src/pages/dashboard/Student/ScanQR.tsx
+++ client/src/pages/dashboard/Student/ScanQR.tsx
@@ -332,7 +332,7 @@ const QRScanner: React.FC = () => {
 
         {/* Cards Grid */}
         <motion.div
-          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+          className="grid grid-cols-2  lg:grid-cols-3 gap-5"
           initial="hidden"
           animate="visible"
           viewport={{ root: scrollRef, amount: 0.2 }}
